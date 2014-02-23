8d6e31521dcea5065c0f428b59a9e3b75cc97ab3
ethannash <ethannash3@gmail.com>
2014-02-23 12:04:27 -0800

    Update PictureResponse.java
    added documentation

--- PictureResponse.java
+++ PictureResponse.java
@@ -6,12 +6,23 @@ public class PictureResponse extends Question {
 	private String question, answer;
 	private int num;
 	
+	/**
+	 * Constructor for PictureResponse
+	 * @param question: A String of the URL for the picture
+	 * @param answer: A String of the answer
+	 * @param num: An int for the number of the question in the quiz
+	 */
 	public PictureResponse(String question, String answer, int num) {
 		this.question = question;
 		this.answer = answer;
 		this.num = num;
 	}
 
+	/**
+	 * getQuestion returns a String of the HTML required to
+	 * display the question. This includes an image and the text
+	 * input for the answer.
+	 */
 	@Override
 	public String getQuestion() {
 		StringBuilder buff = new StringBuilder();
@@ -26,11 +37,18 @@ public class PictureResponse extends Question {
 		return buff.toString();
 	}
 
+	/**
+	 * checkAnswer takes a String answer and returns true if the
+	 * answer is correct, false otherwise.
+	 */
 	@Override
 	public boolean checkAnswer(String answer) {
 		return this.answer.equals(answer);
 	}
 
+	/**
+	 * getAnswer returns the correct answer as a String
+	 */
 	@Override
 	public String getAnswer() {
 		return this.answer;
