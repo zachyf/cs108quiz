30907d9e494f0b9d5ec920a7fa64e7a55fa8e65e
ethannash <ethannash3@gmail.com>
2014-02-23 12:03:33 -0800

    Update FillInTheBlank.java

--- FillInTheBlank.java
+++ FillInTheBlank.java
@@ -5,6 +5,12 @@ public class FillInTheBlank extends Question {
 	private String question, answer;
 	private int num;
 	
+	/**
+	 * Constructor for FillInTheBlank
+	 * @param question: A String of the question
+	 * @param answer: A String of the answer
+	 * @param num: An int for the number of the question in the quiz
+	 */
 	public FillInTheBlank(String question, String answer, int num) {
 		this.question = question;
 		this.answer = answer;
@@ -12,6 +18,11 @@ public class FillInTheBlank extends Question {
 		
 	}
 	
+	/**
+	 * getQuestion returns a String of the HTML required to
+	 * display the question. This includes the question and the text
+	 * input for the answer.
+	 */
 	@Override
 	public String getQuestion() {
 		StringBuilder buff = new StringBuilder();
@@ -24,11 +35,18 @@ public class FillInTheBlank extends Question {
 		return buff.toString();
 	}
 
+	/**
+	 * checkAnswer takes a String answer and returns true if the
+	 * answer is correct, false otherwise.
+	 */
 	@Override
 	public boolean checkAnswer(String answer) {
 		return this.answer.equals(answer);
 	}
 
+	/**
+	 * getAnswer returns the correct answer as a String
+	 */
 	@Override
 	public String getAnswer() {
 		return this.answer;
