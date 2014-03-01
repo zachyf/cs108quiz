eacfda4f917fa538cc74f470d77c5f3fb1d3a822
ethannash <ethannash3@gmail.com>
2014-03-01 03:30:31 -0800

    Update Answer.java
    Created Answer class

--- src/Answer.java
+++ src/Answer.java
@@ -1,9 +1,68 @@
 package quiz;
 
+import java.util.*;
+
+
 public class Answer {
 
-	//StubClass
-	
-	public Answer(){}
-	
+		private int numCorrect;
+		private long startTime;
+		private long timeToComplete;
+		private User user;
+		private Quiz quiz;
+		private HashMap<Question, String> answers;
+		private Date dateCompleted;
+		
+		public Answer(User user, Quiz quiz) {
+			this.user = user;
+			this.quiz = quiz;
+			this.numCorrect = 0;
+		}		
+		
+		public double getScore() {
+			return (double)this.numCorrect/this.answers.size();
+		}
+		
+		public void startTimer() {
+			this.startTime = System.currentTimeMillis();
+		}
+		
+		public void endTimer() {
+			this.timeToComplete = System.currentTimeMillis() - this.startTime;
+			this.dateCompleted = new Date();
+		}
+		
+		public void setAnswer(Question question, String answer) {
+			if (question.checkAnswer(answer)) {
+				this.numCorrect++;
+			}
+			answers.put(question, answer);
+		}
+		
+		public long getTimeToComplete() {
+			return this.timeToComplete;
+		}
+		
+		public User getUser() {
+			return this.user;
+		}
+		
+		public Quiz getQuiz() {
+			return this.quiz;
+		}
+		
+		public Date getDateCompleted() {
+			return this.dateCompleted;
+		}
+		
+		public int compareTo(Answer answer) {
+			double thisScore = this.getScore();
+			double compareScore = answer.getScore();
+			if (thisScore == compareScore) {
+				return (int)(this.timeToComplete - answer.timeToComplete);
+			} else {
+				return (int)(thisScore - compareScore);
+			}
+		}
+		
 }
