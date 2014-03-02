0692dcffa9236273ca546b02975c565b990afb22
zachyf <Zach.Flaherty@gmail.com>
2014-03-02 14:23:19 -0800

    first merge of two subgroups' files. DB and listener is now shared but flow has not yet been integrated

---
+++ userside.sql
@@ -0,0 +1,40 @@
+USE c_cs108_schottj;
+
+DROP TABLE IF EXISTS messages;
+
+CREATE TABLE messages (
+	fromUser CHAR(20),
+    toUser CHAR(20),
+    subject CHAR(64),
+    note CHAR(255),
+    hasRead INT,
+    sentTime TIMESTAMP
+    );
+    
+DROP TABLE IF EXISTS friends;
+
+CREATE TABLE friends (
+	user1 CHAR(64),
+    user2 CHAR(64),
+    );
+    
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+	user_name CHAR(64),
+    password CHAR(64),
+    salt INT,
+    numCreated INT,
+    numPlayed INT,
+    highScore TINYINT(1),
+    practiceMode TINYINT(1),
+    adminStatus TINYINT(1),
+    );
+    
+DROP TABLE IF EXISTS pending;
+
+CREATE TABLE pending (
+	user1 CHAR(64),
+    user2 CHAR(64),
+    pending TINYINT(1)
+    );
