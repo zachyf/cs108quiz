dd4e35de3be33dc3956f691e04fe027d78accd81
thejr <jr.futrell@gmail.com>
2014-03-03 19:47:33 -0800

    Answers and Quiz SQL file updates

--- quiz.sql
+++ quiz.sql
@@ -8,15 +8,9 @@ CREATE TABLE quizzes (
     name VARCHAR(128),
     description TEXT,
     creatorName VARCHAR(64),
-    date DATE,
+    date DATETIME,
     onePage TINYINT(1),
     isRandomOrder TINYINT(1),
     isImmediate TINYINT(1),
     hasPracticeMode TINYINT(1)
     );
-
-
-INSERT INTO quizzes VALUES
-	(0,"Test Quiz 1","This is a test quiz for testing","JR",2013-12-31,0,0,0,0),
-    (1,"Test Quiz 2","This is a test quiz for testing","JR",2014-02-11,0,0,0,0),
-    (2,"Test Quiz 4","This is a test quiz for testing","Ethan",2014-02-12,0,0,0,0)
