48680baca4d9be5ca06dac8205f9ed0584b65dd9
Zachary Yellin-Flaherty <zachyf@717d-cl-02-imt2.stanford.edu>
2014-03-08 21:25:06 -0800

    changed name of answers database to quizRecord. Answer is imprecise and confused me

--- answers.sql
+++ answers.sql
@@ -1,13 +1,13 @@
 USE c_cs108_jfutrell;
 
-DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS quizRecords;
  -- remove table if it already exists and start from scratch
 
-CREATE TABLE answers (
+CREATE TABLE quizRecords (
 	numCorrect SMALLINT(5),
     numQuestions SMALLINT(5),
-    timeToComplete BIGINT(15),
+    timeToComplete BIGINT(15), --in miliseconds
     userName VARCHAR(64),
     quizID SMALLINT(5),
-    date DATETIME
+    timeSubmitted TIMESTAMP
     );
