1a301becb11b6faf2bbf22844504a7a4132c7e2e
ethannash <ethannash3@gmail.com>
2014-03-12 04:43:02 -0700

    More bootstrap for mailbox, quiz history, sent messages, and view quiz

--- src/quiz/SendMessage.java
+++ src/quiz/SendMessage.java
@@ -50,6 +50,42 @@ public class SendMessage extends HttpServlet {
 		String loggedInUser = (String) ses.getAttribute("name");
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\" />");
+		out.println("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
+		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+		out.println("<link href=\"bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">");
+		out.println("<link href=\"bootstrap/css/bootstrap-theme.min.css\" rel=\"stylesheet\">");
+		out.println("<link href=\"css/jumbotron.css\" rel=\"stylesheet\">");
+		out.println("<title>Send Message</title>");
+		out.println("</head>");
+		out.println("<body>");
+		
+		// Nav bar html
+		out.println("<div class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">");
+		out.println("<div class=\"container\"><div class=\"navbar-header\">");
+		out.println("<button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">");
+		out.println("<span class=\"sr-only\">Toggle navigation</span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span>");
+		out.println("</button>");
+		out.println("<a class=\"navbar-brand\" href=\"userWelcome\">Quiz Mania!</a>");
+		out.println("</div><div class=\"navbar-collapse collapse\">");
+		out.println("<ul class=\"nav navbar-nav\">");
+		out.println("<li><a href=\"userWelcome\">Home <span class=\"glyphicon glyphicon-home\"></span></a></li>");
+		out.println("<li><a href=\"quizPerformanceSummary\">My Quiz History <span class=\"glyphicon glyphicon-th-list\"></a></li>");
+		out.println("<li><a href=\"createQuiz.html\">Create Quiz <span class=\"glyphicon glyphicon-pencil\"></a></li>");
+		out.println("<li><a href=\"logout\">Logout <span class=\"glyphicon glyphicon-off\"></a></li>");
+		out.println("</ul>");
+		out.println("<form action=\"SearchQuizzesServlet\" method=\"GET\" class=\"navbar-form navbar-right\" role=\"form\">");
+		out.println("<div class=\"form-group\">");
+		out.println("<input name=\"search\" type=\"text\" placeholder=\"Search Quizzes...\" class=\"form-control\">");
+		out.println("</div><button type=\"submit\" class=\"btn btn-success\">Search <span class=\"glyphicon glyphicon-search\"></button>");
+		out.println("</form>");
+		out.println("</div></div></div>");
+		
+		out.println("<div class=\"container\"><br><br>");
 		try {
 			if(!DB.userExists(to)){
 				out.println("User "+to+" does not exist.");
@@ -65,6 +101,10 @@ public class SendMessage extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		out.println("</div>");
+		
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
--- src/quiz/quizPerformanceSummary.java
+++ src/quiz/quizPerformanceSummary.java
@@ -45,12 +45,41 @@ public class quizPerformanceSummary extends HttpServlet {
 		out.println("<html>");
 		out.println("<head>");
 		out.println("<meta charset=\"UTF-8\" />");
+		out.println("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
+		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+		out.println("<link href=\"bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">");
+		out.println("<link href=\"bootstrap/css/bootstrap-theme.min.css\" rel=\"stylesheet\">");
+		out.println("<link href=\"css/jumbotron.css\" rel=\"stylesheet\">");
 		out.println("<title>"+username+"'s Quiz Performance</title>");
 		out.println("</head>");
 		out.println("<body>");
+
+		// Nav bar html
+		out.println("<div class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">");
+		out.println("<div class=\"container\"><div class=\"navbar-header\">");
+		out.println("<button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">");
+		out.println("<span class=\"sr-only\">Toggle navigation</span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span>");
+		out.println("</button>");
+		out.println("<a class=\"navbar-brand\" href=\"userWelcome\">Quiz Mania!</a>");
+		out.println("</div><div class=\"navbar-collapse collapse\">");
+		out.println("<ul class=\"nav navbar-nav\">");
+		out.println("<li><a href=\"userWelcome\">Home <span class=\"glyphicon glyphicon-home\"></span></a></li>");
+		out.println("<li><a href=\"quizPerformanceSummary\">My Quiz History <span class=\"glyphicon glyphicon-th-list\"></a></li>");
+		out.println("<li><a href=\"createQuiz.html\">Create Quiz <span class=\"glyphicon glyphicon-pencil\"></a></li>");
+		out.println("<li><a href=\"logout\">Logout <span class=\"glyphicon glyphicon-off\"></a></li>");
+		out.println("</ul>");
+		out.println("<form action=\"SearchQuizzesServlet\" method=\"GET\" class=\"navbar-form navbar-right\" role=\"form\">");
+		out.println("<div class=\"form-group\">");
+		out.println("<input name=\"search\" type=\"text\" placeholder=\"Search Quizzes...\" class=\"form-control\">");
+		out.println("</div><button type=\"submit\" class=\"btn btn-success\">Search <span class=\"glyphicon glyphicon-search\"></button>");
+		out.println("</form>");
+		out.println("</div></div></div>");
+		
+		out.println("<div class=\"container\">");
 		out.println("<h2>"+username+"'s Quiz Performance</h2>");
+		
 		if(size>0){
-			out.println("<table>");
+			out.println("<table class=\"table\">");
 			out.println("<tr>");
 			out.println("<th>Quiz</th>");
 			out.println("<th>Score</th>");		
@@ -60,8 +89,7 @@ public class quizPerformanceSummary extends HttpServlet {
 			out.println("<tr>");
 			for(int i = 0; i < myRecentPerformance.size(); i++){
 				String quiz=DB.getQuizAt((Integer)myRecentPerformance.get(i).get(0)).getName();
-				out.println("<td><a href=\"quizPage.jsp?id="+(Integer)myRecentPerformance.get(i).get(0)+"\">"+quiz+"</a></td");
-				out.println("<td>" + myRecentPerformance.get(i).get(2) + "</td>");
+				out.println("<td><a href=\"quizPage.jsp?id="+(Integer)myRecentPerformance.get(i).get(0)+"\">"+quiz+"</a></td>");
 				out.println("<td>" + ((Double)(myRecentPerformance.get(i).get(1))*100) + "%</td>");
 				out.println("<td>" + myRecentPerformance.get(i).get(2) + " seconds </td>");
 				String date = new SimpleDateFormat("HH:mm MM/dd/yyyy").format( myRecentPerformance.get(i).get(3));
@@ -72,7 +100,7 @@ public class quizPerformanceSummary extends HttpServlet {
 		}else{
 			out.println("<h4>You no have quiz history. </h4>");
 		}
-		out.println("<br><a href=\"userWelcome\"><img src=\"home.jpg\" title=\"Return Home \"></img></a>");
+		out.println("</div>");
 		out.println("</body>");
 		out.println("</html>");
 	}
